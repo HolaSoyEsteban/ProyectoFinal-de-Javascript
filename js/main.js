ea7c214cb9f67702562b89a7eb74bca0905ba177
Esteban Cardozo <cardozo.esteban@gmail.com>
2023-02-15 01:33:02 -0300

    Carrito se muestra en el DOM ... Listo!

--- js/main.js
+++ js/main.js
@@ -1,38 +1,7 @@
 // Funciones
 
-/* 
-    <div class="productoVistaTarjeta">
-                    <div class="imagenProducto">
-                        <img src="../img/productos/coleccionLibrosSherlockHolmes.jpg" alt="Libros de Sherlock Holmes">
-                    </div>
-                    
-                    <div id="SagaSherlock">
-                        
-                    </div>
-                    
-                    <div class="botonDeAgregarAlCarrito">
-                        <p>Agregar al carrito</p>
-                    </div>
-    </div>
-*/ 
-
-
 function agregandoCarritoALLocalStorage(){
     localStorage.setItem("carrito", JSON.stringify(carrito));
-}
-
-function carritoDelLocalStorage(){
-    let carrito = [];
-    const carritoLocalStorage = localStorage.getItem("carrito");
-
-    if (carritoLocalStorage !== null){
-        carrito = JSON.parse(carritoLocalStorage);
-    }
-    return carrito;
-}
-
-function mostrarCarritoEnElDOM(productosDelCarrito){
-    console.log(productosDelCarrito);
 }
 
 function agregandoProductosAlCarrito(productoParaAgregar){
@@ -51,6 +20,63 @@ function agregandoProductosAlCarrito(productoParaAgregar){
 
     agregandoCarritoALLocalStorage()
     mostrarCarritoEnElDOM(carrito)
+}
+
+function alertaDeProductoAgregado(){
+    Toastify({
+        text: "Producto Agregado",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        className: "paraAlertas",
+        style: {
+          background: "linear-gradient(to right, #ffe4b5, #91520174)",
+        }
+      }).showToast();
+}
+
+function carritoDelLocalStorage(){
+    let carrito = [];
+    const carritoLocalStorage = localStorage.getItem("carrito");
+
+    if (carritoLocalStorage !== null){
+        carrito = JSON.parse(carritoLocalStorage);
+    }
+    return carrito;
+}
+
+function mostrarCarritoEnElDOM(productosDelCarrito){
+    const contenidoDelCarritoEnElDOM = document.getElementById("contenidoDelCarrito");
+    contenidoDelCarritoEnElDOM.innerHTML = "";
+
+    for (const producto of productosDelCarrito){
+        const detalleProducto = document.createElement("div");
+        detalleProducto.classList.add("contenidoDelDetalle");
+
+        const nombreProducto = document.createElement("p");
+        nombreProducto.innerHTML = `Nombre: ${producto.nombre}`;
+        detalleProducto.append(nombreProducto);
+
+        const precioProducto = document.createElement("p");
+        precioProducto.innerHTML = `Precio: $${producto.precio}`;
+        detalleProducto.append(precioProducto);
+
+        const cantidadProducto = document.createElement("p");
+        cantidadProducto.innerHTML = `Cantidad: `;
+        const valorCantidadProducto = document.createElement("p");
+        valorCantidadProducto.innerHTML = `${producto.cantidad}`;
+        detalleProducto.append(cantidadProducto,valorCantidadProducto);
+
+        const precioTotalProducto = document.createElement("p");
+        precioTotalProducto.innerHTML = `Precio Total: $${producto.precio * producto.cantidad}`;
+        detalleProducto.append(precioTotalProducto);
+
+        contenidoDelCarritoEnElDOM.append(detalleProducto);
+    }
 }
 
 function renderizarProductosEnElHTML(Productos) {
@@ -79,6 +105,7 @@ function renderizarProductosEnElHTML(Productos) {
         divBotonDeAgregarAlCarrito.className = "botonDeAgregarAlCarrito";
         divBotonDeAgregarAlCarrito.addEventListener("click", () => {
             agregandoProductosAlCarrito(producto);
+            alertaDeProductoAgregado();
         });
         const pTextoBotonCarrito = document.createElement("p");
         pTextoBotonCarrito.innerHTML = "Agregar al carrito";
@@ -90,11 +117,21 @@ function renderizarProductosEnElHTML(Productos) {
         divVistaTarjeta.append(divImagenProducto,pNombreProducto,divPrecio,divBotonDeAgregarAlCarrito);
         listadoDeProductosAlHTML.append(divVistaTarjeta);
     }
+}
 
+function VaciarCarrito(){
+    const botonVaciar = document.getElementById("Vaciar");
+    botonVaciar.addEventListener("click", () => {
+        localStorage.clear();
+        location.reload();
+    });
 }
 
 const carrito = carritoDelLocalStorage();
+
+VaciarCarrito();
 mostrarCarritoEnElDOM(carrito);
+
 
 fetch("../js/productos.json")
     .then( (respuestaDelServidor) => {
